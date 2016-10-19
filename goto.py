5ff78a998e3996135267b0ff77c21545a3c45dc5
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2016-10-19 17:17:45 -0400

    Make goto.py computer agnostic
    I re-wrote the goto.py script to make it select the paths based on which
    computer it is run on.  This allows a single script to be used, which
    can be executed from a now general pymolrc.

--- goto.py
+++ goto.py
@@ -1,15 +1,32 @@
 from pymol import cmd
+import platform
 
-def goto(pathname):
-    path_select = {
-        "drive": "E:\Users\Brahm\Documents\Google Drive",
-        "design": "E:\Users\Brahm\Documents\Google Drive\design",
-        "pdbs": "E:\Users\Brahm\Documents\Google Drive\PDB Files",
-        "dropbox": "E:\Users\Brahm\Documents\Dropbox"
-    }
+if (platform.node == "BrahmL")
+	def goto(pathname):
+		path_select = {
+			"drive": "E:\Users\Brahm\Documents\Google Drive",
+			"design": "E:\Users\Brahm\Documents\Google Drive\design",
+			"pdbs": "E:\Users\Brahm\Documents\Google Drive\PDB Files",
+			"dropbox": "E:\Users\Brahm\Documents\Dropbox"
+		}
     
-    os.chdir(path_select.get(pathname, "."))
-    print "Changing to the following directory:"
-    print path_select.get(pathname, ".")
+		os.chdir(path_select.get(pathname, "."))
+		print "Changing to the following directory:"
+		print path_select.get(pathname, ".")
 
-cmd.extend("goto",goto)
+	cmd.extend("goto",goto)
+
+if (platform.node == "Brahm")
+	def goto(pathname):
+		path_select = {
+			"drive": "D:\Users\Brahm Yachnin\Documents\Google Drive",
+			"design": "D:\Users\Brahm Yachnin\Documents\Google Drive\design",
+			"pdbs": "D:\Users\Brahm Yachnin\Documents\Google Drive\PDB Files",
+			"dropbox": "D:\Users\Brahm Yachnin\Documents\Dropbox"
+		}
+    
+		os.chdir(path_select.get(pathname, "."))
+		print "Changing to the following directory:"
+		print path_select.get(pathname, ".")
+
+	cmd.extend("goto",goto)
