288399c0a17ecaa2cbfccfaa665e4d2e00c865ef
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2019-07-24 13:40:21 -0400

    Update scripts to work with python3

--- goto.py
+++ goto.py
@@ -4,29 +4,29 @@ import platform
 if (platform.node() == "BrahmL"):
 	def goto(pathname):
 		path_select = {
-			"drive": "E:\Users\Brahm\Documents\Google Drive",
-			"design": "E:\Users\Brahm\Documents\Google Drive\design",
-			"pdbs": "E:\Users\Brahm\Documents\Google Drive\PDB Files",
-			"dropbox": "E:\Users\Brahm\Documents\Dropbox"
+			"drive": r"E:\Users\Brahm\Documents\Google Drive",
+			"design": r"E:\Users\Brahm\Documents\Google Drive\design",
+			"pdbs": r"E:\Users\Brahm\Documents\Google Drive\PDB Files",
+			"dropbox": r"E:\Users\Brahm\Documents\Dropbox"
 		}
     
 		os.chdir(path_select.get(pathname, "."))
-		print "Changing to the following directory:"
-		print path_select.get(pathname, ".")
+		print("Changing to the following directory:")
+		print(path_select.get(pathname, "."))
 
 	cmd.extend("goto",goto)
 
 if (platform.node() == "Brahm"):
 	def goto(pathname):
 		path_select = {
-			"drive": "D:\Users\Brahm Yachnin\Documents\Google Drive",
-			"design": "D:\Users\Brahm Yachnin\Documents\Google Drive\design",
-			"pdbs": "D:\Users\Brahm Yachnin\Documents\Google Drive\PDB Files",
-			"dropbox": "D:\Users\Brahm Yachnin\Documents\Dropbox"
+			"drive": r"D:\Users\Brahm Yachnin\Documents\Google Drive",
+			"design": r"D:\Users\Brahm Yachnin\Documents\Google Drive\design",
+			"pdbs": r"D:\Users\Brahm Yachnin\Documents\Google Drive\PDB Files",
+			"dropbox": r"D:\Users\Brahm Yachnin\Documents\Dropbox"
 		}
     
 		os.chdir(path_select.get(pathname, "."))
-		print "Changing to the following directory:"
-		print path_select.get(pathname, ".")
+		print("Changing to the following directory:")
+		print(path_select.get(pathname, "."))
 
 	cmd.extend("goto",goto)
