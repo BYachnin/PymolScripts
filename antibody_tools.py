93831918d8acfb9a4af4b0a58d935f5a938ac038
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2019-08-14 11:28:55 -0400

    Added all_cdr selection and fixed multi-object selections

--- antibody_tools.py
+++ antibody_tools.py
@@ -39,28 +39,32 @@ NOTES
 
 
     """
-    #Figure out which objects to work on.
-    #If the argument object is 'all', work on all objects.
-    #If the argument object is a specific object that is already present, work on that.
-    #Otherwise, assume it is a comma-delimited list and go from there.
-    if object == "all":
-        objects = cmd.get_object_list()
-    elif object in cmd.get_object_list():
-        objects = [object]
-    else:
-        objects = ",".split(object)
-
     #Get the appropriate CDR definitions
     cdrs = {
         'NORTH-AHO': NorthAHo
     }
 
-    cdr = cdrs[cdr_def.upper()]
-    print(cdr.H1_START)
-    print(cdr.H1_START.value)
+    #Figure out which objects to work on.
+    #If the argument object is 'all', work on all objects.
+    #If the argument object is a specific object that is already present, work on that.
+    #Otherwise, assume it is a slash-delimited list and go from there.
+    if object == "all":
+        objects = cmd.get_object_list()
+    elif object in cmd.get_object_list():
+        objects = [object]
+    else:
+        objects = object.split('/')
 
-    print(object)
-    print(objects)
+    #Verify that all items in objects are current pymol objects
+    if not all( obj in cmd.get_object_list() for obj in objects ):
+        raise Exception('One or more objects do not exist in your current PyMOL session.')
+
+    #Verify that cdr_def is a supported CDR definition
+    if not cdr_def.upper() in cdrs.keys():
+        raise Exception('You specified an unsupported CDR definition.  Currently supported definitions include ' + str(list(cdrs.keys()))  )
+
+    cdr = cdrs[cdr_def.upper()]
+
     for obj in objects:
         cmd.select(obj + "_H1", "chain {0} and resi {1}-{2}".format(heavy, cdr.H1_START.value, cdr.H1_END.value))
         cmd.select(obj + "_H2", "chain {0} and resi {1}-{2}".format(heavy, cdr.H2_START.value, cdr.H2_END.value))
@@ -71,6 +75,8 @@ NOTES
         cmd.select(obj + "_L2", "chain {0} and resi {1}-{2}".format(light, cdr.L2_START.value, cdr.L2_END.value))
         cmd.select(obj + "_L3", "chain {0} and resi {1}-{2}".format(light, cdr.L3_START.value, cdr.L3_END.value))
         cmd.select(obj + "_Lcdrs", "{0}_L1 or {0}_L2 or {0}_L3".format(obj))
+
+        cmd.select(obj + "_allcdrs", "{0}_Lcdrs or {0}_Hcdrs".format(obj))
 
 cmd.extend('select_cdrs', select_cdrs)
 # Configure the seq_object argument to be a selection when tab completing.
