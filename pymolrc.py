30b3c817044725d5c3a2e26f490158e7a95d1d9d
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2019-09-11 12:40:36 -0400

    Try to establish pymol-pyrosetta link in pymolrc
    Will figure out the correct version based on pymol's python, and will
    look for various environment variables to find Rosetta.

--- pymolrc.py
+++ pymolrc.py
@@ -24,3 +24,28 @@ cmd.do("run https://raw.githubusercontent.com/BYachnin/PymolScripts/master/color
 cmd.do("run https://raw.githubusercontent.com/BYachnin/PymolScripts/master/design_movie.py")
 cmd.do("run https://raw.githubusercontent.com/BYachnin/PymolScripts/master/loadBfacts.py")
 cmd.do("run https://raw.githubusercontent.com/BYachnin/PymolScripts/master/ray_tracer.py")
+
+#Try to set up a pyrosetta link
+import sys,os
+main_to_pyrsrv = '/source/src/python/PyRosetta/src/' # How to get from main to the location of the PyMOL-RosettaServer files
+
+#Figure out if we should use the python2 or python3 version
+if sys.version_info[0] == 2:
+    pyr_scpt = "PyMOL-RosettaServer.py"
+elif sys.version_info[0] == 3:
+    pyr_scpt = "PyMOL-RosettaServer.python3.py"
+
+#Check the environment variables for Rosetta
+if 'ROSETTA' in os.environ:
+    pyr_scpt_path = os.environ['ROSETTA'] + '/main' + main_to_pyrsrv
+elif 'ROSDB' in os.environ:
+    pyr_scpt_path = os.environ['ROSDB'] + '/..' + main_to_pyrsrv
+elif 'ROSETTA3_DB' in os.environ:
+    pyr_scpt_path = os.environ['ROSETTA3_DB'] + '/..' + main_to_pyrsrv
+
+#If we have a route to the PyMOL-RosettaServer files and we can find a file at that location
+if pyr_scpt_path and os.path.exists(pyr_scpt_path + pyr_scpt):
+    #Run the script in PyMOL
+    cmd.do("run " + pyr_scpt_path + pyr_scpt)
+
+#End of pyrosetta link stuff
